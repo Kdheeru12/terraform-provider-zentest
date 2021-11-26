69bf0a1510c705df9a567bb71491ed93194f079e
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2021-11-26 13:52:15 +0530

    publish 2

--- main.go
+++ main.go
@@ -1,25 +1,7 @@
-// package main
-
-// import (
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-
-// 	"terraform-provider-zenduty/zenduty"
-
-// 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-// )
-
-// func main() {
-// 	plugin.Serve(&plugin.ServeOpts{
-// 		ProviderFunc: func() terraform.ResourceProvider {
-// 			return zenduty.Provider()
-// 		},
-// 	})
-// }
-
 package main
 
 import (
-	"terraform-provider-zenduty/zenduty"
+	"terraform-provider-zendutytest/zenduty"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
