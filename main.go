2b5ddb176059ecce03172f63859bf7af1bc4db54
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2021-11-26 15:10:35 +0530

    publish 4

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"terraform-provider-zentest/zenduty"
+	"terraform-provider-zentest/zentest"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
@@ -10,7 +10,7 @@ import (
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return zenduty.Provider()
+			return zentest.Provider()
 		},
 	})
 }
--- zentest/provider_test.go
+++ zentest/provider_test.go
@@ -1,4 +1,4 @@
-package zenduty
+package zentest
 
 import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
